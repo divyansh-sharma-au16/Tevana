b02022af022c4c14fc8dbc1b163454622b61c3b0
divyansh_sharma_au16 <divyanshsharma7765@gmail.com>
2021-06-10 03:24:32 +0530

    stripe bug

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const stripe = require('stripe');
 const cors = require('cors');
+const csp = require('express-csp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/globalErrorHndler');
@@ -33,7 +34,67 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //global middlewares
 //set security HTTP
+// app.use(helmet());
+
 app.use(helmet());
+
+csp.extend(app, {
+  policy: {
+    directives: {
+      'default-src': ['self'],
+      'style-src': ['self', 'unsafe-inline', 'https:'],
+      'font-src': ['self', 'https://fonts.gstatic.com'],
+      'script-src': [
+        'self',
+        'unsafe-inline',
+        'data',
+        'blob',
+        'https://js.stripe.com',
+        'https://bundle.js:8828',
+        'ws://localhost:56558/',
+      ],
+      'worker-src': [
+        'self',
+        'unsafe-inline',
+        'data:',
+        'blob:',
+        'https://*.stripe.com',
+        'https://*.mapbox.com',
+        'https://*.cloudflare.com/',
+        'https://bundle.js:*',
+        'ws://localhost:*/',
+      ],
+      'frame-src': [
+        'self',
+        'unsafe-inline',
+        'data:',
+        'blob:',
+        'https://*.stripe.com',
+        'https://bundle.js:*',
+        'ws://localhost:*/',
+      ],
+      'img-src': [
+        'self',
+        'unsafe-inline',
+        'data:',
+        'blob:',
+        'https://*.stripe.com',
+        'https://bundle.js:*',
+        'ws://localhost:*/',
+      ],
+      'connect-src': [
+        'self',
+        'unsafe-inline',
+        'data:',
+        'blob:',
+        // 'wss://<HEROKU-SUBDOMAIN>.herokuapp.com:<PORT>/',
+        'https://*.stripe.com',
+        'https://bundle.js:*',
+        'ws://localhost:*/',
+      ],
+    },
+  },
+});
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,11 +1,11 @@
-const stripe = Stripe(
-  'pk_test_51Iz4jeSJZMM3bl9Qllm7TZzswGDn3ZVgzCRxOH7FAU7wUiuM2ugDNmM4p4M0EDvLPpAbopEd0P8X0LjA1dmnI7eD00qrpw7Ytv'
-);
 import axios from 'axios';
 import { showAlert } from './alerts';
 // import {loadStripe} from '@stripe/stripe-js';
 
 export const bookGarden = async (gardenId) => {
+  const stripe = Stripe(
+    'pk_test_51Iz4jeSJZMM3bl9Qllm7TZzswGDn3ZVgzCRxOH7FAU7wUiuM2ugDNmM4p4M0EDvLPpAbopEd0P8X0LjA1dmnI7eD00qrpw7Ytv'
+  );
   try {
     // this.stripe = await loadStripe("pk_test_51Iz4jeSJZMM3bl9Qllm7TZzswGDn3ZVgzCRxOH7FAU7wUiuM2ugDNmM4p4M0EDvLPpAbopEd0P8X0LjA1dmnI7eD00qrpw7Ytv");
     // 1) Get checkout session from API
